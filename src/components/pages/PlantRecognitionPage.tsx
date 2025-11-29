72182ba9f8c4247d0411e0ca45186c43aa94f47e
Withley <abbaslifarhad7@gmail.com>
2025-11-29 10:13:01 +0400

    Add only assets folder

--- src/components/pages/PlantRecognitionPage.tsx
+++ src/components/pages/PlantRecognitionPage.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'motion/react';
 import { Upload, Camera, Loader2, Droplet, Sun, AlertTriangle, Leaf, Sparkles, CheckCircle, MessageCircle, X, Send, Paperclip } from 'lucide-react';
 import { translations, type Language } from '../../utils/translations';
-import telegramIcon from 'src/assets/0b2433cc304d24f11b1e266d5d34bf8456d729d3.png';
+import telegramIcon from 'src/assets/0b2433cc304d24f11b1e266d5d34bf8456d729d3.pn';
 
 interface PlantInfo {
   name: string;
